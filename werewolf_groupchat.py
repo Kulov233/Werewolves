9e93b3b0343de019bbab7e91510ce3164ae24579
anAtheist <hutianshuo2017@163.com>
2024-11-12 21:10:49 +0800

    基础类设计，prompt开发中

--- werewolf_groupchat.py
+++ werewolf_groupchat.py
@@ -1,6 +1,7 @@
 from autogen import ConversableAgent, GroupChat, GroupChatManager
 from keys import ZHIPUAI_API_KEY
-
+from typing import Literal
+from prompts import Prompts
 
 config_list = [
     {
@@ -11,108 +12,43 @@ config_list = [
     }
 ]
 
-class Character(ConversableAgent):
-    def __init__(self, name: str, personality: str = "你是一名狼人杀玩家。"):
+llm_config={
+        "config_list": config_list,
+}
+
+
+class Player:
+    def __init__(self, index: int):
+        self.index = index
+        self.character = None
+
+    def set_character(self, character: Literal["平民","狼人","预言家"]):
+        self.character = character
+
+class AIPlayer(Player):
+    def __init__(self, index:int, name: str,
+                 personality: str = "你是一名狼人杀玩家。"):
         """
         :param name: 角色名称
         :param personality: 性格prompt
         """
-        super().__init__(name, human_input_mode="NEVER", llm_config=config_list)
+        super().__init__(index)
         self.personality = personality
+        self.type = 'AI'
+        self.prompt_generator = Prompts()
+
+    def get_prompt(self):
+        self.prompt_generator.generate_system_prompt()
+    def talk(self):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, index:int, name: str):
+        super().__init__(index)
+        self.type = 'HUMAN'
+    def talk(self):
+        pass
 
 
 
 
-
-def custom_speaker_selection_func(
-    last_speaker: Character,
-    groupchat: autogen.GroupChat
-) -> Union[Agent, Literal['auto', 'manual', 'random' 'round_robin'], None]:
-
-    """Define a customized speaker selection function.
-    A recommended way is to define a transition for each speaker in the groupchat.
-
-    Parameters:
-        - last_speaker: Agent
-            The last speaker in the group chat.
-        - groupchat: GroupChat
-            The GroupChat object
-    Return:
-        Return one of the following:
-        1. an `Agent` class, it must be one of the agents in the group chat.
-        2. a string from ['auto', 'manual', 'random', 'round_robin'] to select a default method to use.
-        3. None, which indicates the chat should be terminated.
-    """
-    pass
-
-groupchat = GroupChat(
-    speaker_selection_method=custom_speaker_selection_func,
-    ...,
-)
-
-
-gpt4_config = {
-    "cache_seed": 42,  # change the cache_seed for different trials
-    "temperature": 0,
-    "config_list": config_list,
-    "timeout": 120,
-}
-
-initializer = autogen.UserProxyAgent(
-    name="Init",
-)
-
-coder = autogen.AssistantAgent(
-    name="Retrieve_Action_1",
-    llm_config=gpt4_config,
-    system_message="""You are the Coder. Given a topic, write code to retrieve related papers from the arXiv API, print their title, authors, abstract, and link.
-You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
-Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
-If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
-""",
-)
-executor = autogen.UserProxyAgent(
-    name="Retrieve_Action_2",
-    system_message="Executor. Execute the code written by the Coder and report the result.",
-    human_input_mode="NEVER",
-    code_execution_config={
-        "last_n_messages": 3,
-        "work_dir": "paper",
-        "use_docker": False,
-    },  # Please set use_docker=True if docker is available to run the generated code. Using docker is safer than running the generated code directly.
-)
-scientist = autogen.AssistantAgent(
-    name="Research_Action_1",
-    llm_config=gpt4_config,
-    system_message="""You are the Scientist. Please categorize papers after seeing their abstracts printed and create a markdown table with Domain, Title, Authors, Summary and Link""",
-)
-
-
-def state_transition(last_speaker, groupchat):
-    messages = groupchat.messages
-
-    if last_speaker is initializer:
-        # init -> retrieve
-        return coder
-    elif last_speaker is coder:
-        # retrieve: action 1 -> action 2
-        return executor
-    elif last_speaker is executor:
-        if messages[-1]["content"] == "exitcode: 1":
-            # retrieve --(execution failed)--> retrieve
-            return coder
-        else:
-            # retrieve --(execution success)--> research
-            return scientist
-    elif last_speaker == "Scientist":
-        # research -> end
-        return None
-
-
-groupchat = GroupChat(
-    agents=[initializer, coder, executor, scientist],
-    messages=[],
-    max_round=20,
-    speaker_selection_method=state_transition,
-)
-manager = GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
